0a1147084d431237bca02b3a1b5a61d91a4709db
abdelrahmanalaa123 <abdulrahmanalaa497@gmail.com>
2025-01-06 13:43:19 +0200

    fix: type_validation feature: metadata_creation

--- metadata/metadata_creation.sh
+++ metadata/metadata_creation.sh
@@ -1,26 +1,50 @@
 #!/bin/bash
 
-metadata_creation () {
-	table_name=$1
-	declare -i col_amount=$2
+. ../type_validation.sh
 
-	if [ col_amount -le 0 ]
+# metadata_creation needs to be ran inside the source of database 
+# assuming it wont be ran unless you're connected where cd'd inside the database
+metadata_creation () {
+	
+	table_name=$1
+	col_amount=$2
+
+	if [[ $(validate_type int $col_amount) = "NaN" ]]
 	then
-		echo "please enter the correct amount of "
+		echo "please enter a valid integer "
+		exit 1
 	fi
-	echo $col_amount
 	shift 2
 
-	col_list=${@:1:$col_amount}
+	echo "size is ${#@}"
+
+	echo "valid size is $(((2*$col_amount)+1))"
+
+	if [ ${#@} -ne $(((2*$col_amount)+1)) ]
+	then
+		echo "please enter a valid amount of columns with their respective column types"
+		exit 1	
+	fi	
+	
+	#converting the seperationg to a list explicitly
+	declare -a col_list=(${@:1:$col_amount})
 	shift $(($col_amount))
 
-	val_list=${@:1:$col_amount}
+	#converting the seperationg to a list explicitly
+	declare -a val_list=(${@:1:$col_amount})
+	shift $(($col_amount))
 	
-	metadata_string="=================\n$table_name\n"
+	metadata_string=$(printf "================\ntable: $table_name\n")
 
 	col_string=""
 	for elem in ${col_list[@]}
 	do
+
+		if [[ $col_string =~ ^(int|string|float)$ ]]
+		then
+			echo "please enter a valid column name"
+			exit 1
+		fi
 		if [[ -n $col_string ]]
 		then
 			col_string+=","
@@ -28,28 +52,48 @@ metadata_creation () {
 		col_string+="$elem"
 
 	done
-	metadata_string+="$col_string\n"
+	metadata_string+=$(printf "\ncolumn_names: $col_string\n")
 	
-	echo "col string is $col_string"
 	val_string=""
 	for elem in ${val_list[@]}
 	do
 
-		if [[ -n $val_string  ]]
+		if [[ $elem =~ ^(int|string|float)$ ]]
 		then
-			val_string+=","
+			if [[ -n $val_string  ]] 
+			then
+				val_string+=","
+			fi
+			val_string+="$elem"
+		else
+			echo "please enter a valid type out of int | string | float"
+			exit 1
 		fi
-		val_string+="$elem"
 
 	done
-	metadata_string+="$val_string\n"
+	metadata_string+=$(printf "\ncolumn_types: $val_string\n")
 
-	echo "val string is $val_string"
-	echo $metadata_string
+
+	if [[ $(validate_type int $1) != "NaN" ]]
+	then
+		if [[ $1 -gt  0 ]] && [[ $1 -le $col_amount ]]
+		then
+			index=$1
+			metadata_string+=$(printf "\nprimary key Index: $index\n")	
+			metadata_string+=$(printf "\nprimary key: ${col_list[index]}\n")	
+		else
+			echo "please enter a valid primary key index"
+			exit 1
+		fi
+	else
+		echo "please enter a valid primary key index starting from 0"
+		exit 1
+	fi
+	echo "$metadata_string" >> "./meta"
 }
 
 
-metadata_creation elem b bla mar ibla qbla bla bkbla
+metadata_creation table 3 bla mar ibla int int int 1
 
 # metadata sample input is database name
 # table_name col_no col_list type_list  
